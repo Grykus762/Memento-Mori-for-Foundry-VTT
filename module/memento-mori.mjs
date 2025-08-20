05cb4578b758012fd3481f6acdde8c978aae0ab2
Luis L <LLeemon762@gmail.com>
2025-08-20 10:34:20 -0700

    Added React Router and initial Route for System Loader

--- module/memento-mori.mjs
+++ module/memento-mori.mjs
@@ -6,7 +6,7 @@ import { BoilerplateActorSheet } from './sheets/actor-sheet.mjs';
 import { BoilerplateItemSheet } from './sheets/item-sheet.mjs';
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from './helpers/templates.mjs';
-import { BOILERPLATE } from './helpers/config.mjs';
+import { MEMENTOMORI } from './helpers/config.mjs';
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -16,14 +16,16 @@ Hooks.once('init', function () {
   // Add utility classes to the global game object so that they're more easily
   // accessible in global contexts.
   console.warn("Initializing Memento Mori")
+  console.log("Logging MEMENTOMORI constants")
+  console.log(MEMENTOMORI)
   game.mementomori = {
     MementoMoriActor,
-    BoilerplateItem,
-    rollItemMacro,
+    //BoilerplateItem,
+    //rollItemMacro,
   };
 
   // Add custom constants for configuration.
-  CONFIG.BOILERPLATE = BOILERPLATE;
+  CONFIG.MEMENTOMORI = MEMENTOMORI;
 
   /**
    * Set an initiative formula for the system
@@ -35,7 +37,7 @@ Hooks.once('init', function () {
   };
 
   // Define custom Document classes
-  CONFIG.Actor.documentClass = BoilerplateActor;
+  CONFIG.Actor.documentClass = MementoMoriActor;
   CONFIG.Item.documentClass = BoilerplateItem;
 
   // Active Effects are never copied to the Actor,
@@ -44,15 +46,26 @@ Hooks.once('init', function () {
   CONFIG.ActiveEffect.legacyTransferral = false;
 
   // Register sheet application classes
+  /**
+   * The Actors global is deprecated and will be removed in Version 15. It now falls under:
+   *  foundry.documents.collections.Actors
+   */
+  const Actors = foundry.documents.collections.Actors
   Actors.unregisterSheet('core', ActorSheet);
   Actors.registerSheet('boilerplate', BoilerplateActorSheet, {
     makeDefault: true,
     label: 'BOILERPLATE.SheetLabels.Actor',
   });
+
+    /**
+   * The Items global is deprecated and will be removed in Version 15. It now falls under:
+   *  foundry.documents.collections.Items
+   */
+  const Items = foundry.documents.collections.Items
   Items.unregisterSheet('core', ItemSheet);
   Items.registerSheet('boilerplate', BoilerplateItemSheet, {
     makeDefault: true,
-    label: 'BOILERPLATE.SheetLabels.Item',
+    label: 'MEMENTOMORI.SheetLabels.Item',
   });
 
   // Preload Handlebars templates.
